9049ae134ffbf2f4a696d5ec1a55653626c9e0fc
Brandon Pittman <brandon@blp.is>
2024-04-04 15:49:05 +0900

    add context helpers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
-export { readFlash, writeFlash, readFlashMessage, flashMessage } from "./flash"
+import {
+  createContextId,
+  useContext,
+  useContextProvider,
+} from "@builder.io/qwik"
+import type { Cookie } from "@builder.io/qwik-city"
 
-export type { MessageVariant } from "./flash"
+const FLASH_MESSAGE = "flash-message"
+const flash = (name: string) => `__${name}__`
+
+export const readFlash = (cookie: Cookie, name: string) => {
+  const flashedName = flash(name)
+  let flashValue
+
+  if (cookie.has(flashedName)) {
+    flashValue = cookie.get(flashedName)
+    cookie.delete(flashedName, { path: "/" })
+  }
+
+  return flashValue
+}
+
+export const writeFlash = (cookie: Cookie, name: string, value: any) => {
+  cookie.set(flash(name), value, { path: "/", httpOnly: true })
+}
+
+export type MessageVariant = "info" | "success" | "error"
+
+export const readFlashMessage = (cookie: Cookie) =>
+  readFlash(cookie, FLASH_MESSAGE)?.json() as
+    | {
+        value: string
+        type: MessageVariant
+      }
+    | undefined
+
+export const flashMessage = (
+  cookie: Cookie,
+  value: string,
+  type?: MessageVariant,
+) => {
+  writeFlash(cookie, FLASH_MESSAGE, { value, type })
+}
+
+export type FlashContextStore = {
+  value?: string
+  type?: MessageVariant
+}
+export const FlashContext = createContextId<FlashContextStore>("FLASH_PROVIDER")
+
+export const useFlashProvider = (store: FlashContextStore) =>
+  useContextProvider(FlashContext, store)
+
+export const useFlashContext = useContext(FlashContext)
